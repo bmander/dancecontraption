dd9e9ee2c14414805b0628e4718141a383d6fc29
Brandon Martin-Anderson <badhill@gmail.com>
2011-01-26 15:13:36 -0800

    decouple urls

--- urls.py
+++ urls.py
@@ -3,10 +3,7 @@ from django.conf.urls.defaults import *
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
-    (r'^polls/$', 'polls.views.index'),
-    (r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    (r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+    (r'^polls/', include('polls.urls')),
 
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     ('^$', 'django.views.generic.simple.direct_to_template',
