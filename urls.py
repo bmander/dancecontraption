36cec91ceb36a806b5ef9e18a05b2e227e2464a0
Brandon Martin-Anderson <badhill@gmail.com>
2011-01-26 15:49:50 -0800

    following django tutorial

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
-    (r'^polls/', include('polls.urls')),
+    (r'^', include('polls.urls')),
 
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     ('^$', 'django.views.generic.simple.direct_to_template',
