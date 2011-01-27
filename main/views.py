f21b07ccc625dbec5e4326cfb0486cf9687ef77a
Brandon Martin-Anderson <badhill@gmail.com>
2011-01-26 16:14:33 -0800

    added basic index template

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    return render_to_response('main/index.html', {})
--- settings.py
+++ settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'djangotoolbox',
+    'main',
 
     # djangoappengine should come last, so it can override a few manage.py commands
     'djangoappengine',
