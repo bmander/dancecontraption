4bb211dc3b9c2469d83cad4093a21d076412d0b8
Brandon Martin-Anderson <badhill@gmail.com>
2011-01-31 18:54:08 -0800

    dance template

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import *
 
+from main.views import dance
+
 urlpatterns = patterns('main.views',
     (r'^$', 'index'),
+    url(r'dance/(?P<id>\d+)/$', dance, name='dance')
 )
--- main/views.py
+++ main/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
+from main.models import Dance
+
 def index(request):
-    return render_to_response('main/index.html', {})
+    dances = Dance.objects.all() 
+
+    return render_to_response('main/index.html', {'dances':dances})
+
+def dance(request, id):
+    dance = Dance.objects.get(pk=id)
+
+    return render_to_response('main/dance.html', {'dance':dance})
