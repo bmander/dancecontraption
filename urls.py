dc0c10ac152fc2fbfe16ab644f2f03fcedff3edc
Brandon Martin-Anderson <badhill@gmail.com>
2011-02-10 20:03:29 -0800

    login url

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import *
+from django.contrib.auth.views import login
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
+    (r'^login/$', login ),
     (r'^', include('main.urls')),
 )
