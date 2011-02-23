f26dc7cbe84322b9078bcae71042ce87732cace3
Brandon Martin-Anderson <badhill@gmail.com>
2011-02-23 00:48:00 -0800

    use django's logout view

--- urls.py
+++ urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.views import login
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
-    (r'^login/$', 'django.contrib.auth.views.login' ), #try using django.contrib.auth.views.login instead
-    (r'^logout/$', 'main.views.logout' ),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login' ), #try using django.contrib.auth.views.login instead
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login' ),
     (r'^accounts/profile/$', 'main.views.profile' ),
     (r'^', include('main.urls')),
 )
