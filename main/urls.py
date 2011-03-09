27392f12d62ac20a9d251aeeecf3110129deb826
Brandon Martin-Anderson <badhill@gmail.com>
2011-03-08 20:46:01 -0800

    cleaned up urls a bit

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls.defaults import *
 
-from main.views import dance, dance_add
-
 urlpatterns = patterns('main.views',
     (r'^$', 'index'),
-    url(r'dance/add/$', dance_add),
-    url(r'dance/(?P<id>\d+)/$', dance, name='dance'),
-    url(r'band/(?P<id>\d+)/$', 'band' ),
+    url(r'^dance/add/$', 'dance_add'),
+    url(r'^dance/(?P<id>\d+)/$', 'dance', name='dance'),
+    url(r'^band/(?P<id>\d+)/$', 'band' ),
     url(r'^accounts/profile/$', 'profile' ),
     url(r'^accounts/signup/$', 'signup', name="signup" ),
     url(r'^accounts/admin/sethome/$', 'set_home_dance',),
